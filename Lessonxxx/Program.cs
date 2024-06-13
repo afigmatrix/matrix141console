6a0f9d40aa938160dd361c864de45c3467112355
Hemid Ismayilov <gamid.ismailov.001@gmail.com>
2024-06-14 00:18:40 +0400

    salam

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,47 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Check
+            //Console.WriteLine("Soz daxil edin");
+            //string soz= Console.ReadLine();
+            //Console.WriteLine(Check(soz));
+            #endregion
+            #region 10simvol
+            //string soz2 = input();
+            //Console.WriteLine("True");
+            #endregion
+
         }
+        #region Check
+        //static string Check(string soz)
+        //{
+        //    if (soz.StartsWith("A") || soz.StartsWith("a"))
+        //        return "true";
+        //    else if (soz.StartsWith("B") || soz.StartsWith("b"))
+        //        return "true";
+        //    else if (soz.StartsWith("C") || soz.StartsWith("c"))
+        //        return "true";
+        //    else
+        //        return "false";
+        //}
+        #endregion
+        #region 10simvol
+        //static string input()
+        //{
+        //    while (true)
+        //    {
+        //        Console.WriteLine("10 simvoldan artiq soz daxil edin");
+        //        string soz2 = Console.ReadLine();
+        //        if(soz2.Length > 10)
+        //        {
+        //            return soz2;
+        //        }
+        //        else
+        //        Console.WriteLine("daxil etdiyiniz soz 10 simvoldan artiq olmalidi");
+        //    }
+        //}
+        #endregion
     }
+
 }
+
