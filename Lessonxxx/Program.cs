b86681b54b3bcdb55fd325b612dae1c094c1ce12
Emin <emin@example.com>
2024-06-13 20:27:57 +0400

    task bitdi

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,46 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         
+            //Console.WriteLine("Soz daxil edin!");
+            //string soz = Console.ReadLine();
+            // bool result = Method(soz);
+            // Console.WriteLine(result);
+
+
+           
+
+            Console.WriteLine("Deyer daxil edin");
+
+            while (true)
+            {
+                string deyer = Console.ReadLine();
+                if (deyer.Length <= 10)
+                {
+                    Console.WriteLine("Deyer 10dan kicikdir yeniden daxil edin");
+                }
+                else if (deyer.Length > 10)
+                {
+                    Console.WriteLine($"Daxil etdiyiniz deyer = {deyer}");
+                    break;
+                }
+
+            }
+
         }
     }
+
 }
+//        static bool Method (string name)
+//        {
+//            if (name.StartsWith('a') || name.StartsWith('b') || name.StartsWith('c'))
+//            {
+//                return true;
+//            }
+//            else
+//                return false;
+
+//        }
+//    }  
+//}
+
