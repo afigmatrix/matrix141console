2d9fba53507b7fdf5257ede222c1f86d81a1abe8
TeymurAkhmadov <teymur.ahmadov96@gmail.com>
2024-06-13 12:09:17 +0400

    commit -m  "T-1  T-2"

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -4,12 +4,45 @@ namespace Lessonxxx
 {
     internal class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    //T-1
+        //    Console.WriteLine("Enter the string value");
+        //    string Holding = Console.ReadLine();
+        //    Console.WriteLine(St(Holding));
+
+        //}
+
+        //static bool St(string Holding)
+        //{
+        //    if (Holding.StartsWith ("A") || Holding.StartsWith ("B") || Holding.StartsWith("C"))
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+
+        //T-2
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Test1");
-            Console.WriteLine("Test2");
+            Console.WriteLine("Enter a value greater than 10 in length");
+            string gala = Console.ReadLine();
+            Test(gala);
+        }
 
+        static void Test(string gala)
+        {
+            while(gala.Length < 10)
+            {
+                Console.WriteLine("Enter a valu greater than in length");
+                gala = Console.ReadLine();
+            }
+
+            Console.WriteLine("You have entered the correct value: " + gala);
         }
     }
 }
