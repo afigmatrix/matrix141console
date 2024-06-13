ff97c5bbf332224bb6ad03b2089e073f28553ace
Turkay Novruzlu <tnovruzlu7682@gmail.com>
2024-06-14 03:47:03 +0400

    task done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lessonxxx
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            
-        }
+        #region task1
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Texti daxil edin:");
+        //    string text = Console.ReadLine();
+        //    bool result = Method(text);
+        //    Console.WriteLine(result);
+        //}
+        //static bool Method(string text)
+        //{
+        //    bool result = default;
+        //    if(text.StartsWith("A")|| text.StartsWith("b")|| text.StartsWith("C"))
+        //    {
+        //        result = true;
+        //    }
+        //    else
+        //    {
+        //        result = false;
+        //    }
+        //    return result;
+        //}
+        #endregion
+        #region task 2
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Texti daxil edin:");
+        //    string text = Console.ReadLine();
+        //    while (text.Length < 10)
+        //    {
+        //        Console.Write("Texti yeniden daxil edin:");
+        //        text = Console.ReadLine();
+        //    }
+        //    Console.WriteLine($"Dogru deyer daxil etdiniz.Daxil etdiyiniz deyer:{text}-dir");
+        //}
+        #endregion
     }
 }
