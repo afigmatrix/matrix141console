be87197a167c33a22d3ea4fbc3c8796bccb1cc60
Afig Matrix <afig.matrix@gmail.com>
2024-06-14 19:58:55 +0400

    Done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Lessonxxx
 {
@@ -6,7 +7,82 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            doWhile();
+
         }
+
+
+        #region do-while
+        static void doWhile()
+        {
+            //string value = Console.ReadLine();
+            //if (value.Length < 10)
+            //{
+            //    while (true)
+            //    {
+            //        string value2 = Console.ReadLine();
+            //        if (value2.Length > 10)
+            //        {
+            //            Console.WriteLine("Value is valid");
+            //            break;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Value is valid without while");
+            bool run = default;
+            do
+            {
+                string value2 = Console.ReadLine();
+                if (value2.Length > 10)
+                {
+                    Console.WriteLine("Value is valid");
+                    break;
+                }
+            }
+            while (run);
+          
+
+        }
+        #endregion
+        #region SwichWhen
+        static void SwichWhen()
+        {
+            string name = "Matrix";
+            switch (name)
+            {
+                case string name2 when name.StartsWith("M"):
+                    Console.WriteLine("Name start with M");
+                    break;
+            }
+        }
+        #endregion
+        #region Continue
+        static void Continue()
+        {
+            int count = 40;
+            for (int i = 0; i < count; i++)
+            {
+                if (i == 25 || i == 30)
+                {
+                    return;
+                }
+                Console.WriteLine(i);
+            }
+        }
+        #endregion
     }
 }
+
+
+
+//switch (caseSwitch)
+//{
+//    ...
+//    case TypeA myVar when myVar.Size > 0:
+//    ...
+//        break;
+//case < type> < variable_name > when<any_boolean_expression>:
+//        ...
+//        break;
+//    ...
+//}
