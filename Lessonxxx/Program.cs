6808b2cde216b921d6d51025b64eeefa1e4a01e7
Afig Matrix <afig.matrix@gmail.com>
2024-06-21 20:55:15 +0400

    Done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Lessonxxx
 {
@@ -6,7 +7,74 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] myArr = new int[9];
+            //Array.Resize(ref myArr, 15 );
+            //for (int i = 0; i < myArr.Length; i++)
+            //{
+            //    Console.WriteLine(myArr[i]); //get
+            //}
+
+
+            //string name = "Mat";
+            //char M = 'M';
+            //char a = 'a';
+            //char t = 't';
+
+            //char[] charArr = name.ToCharArray();
+            //for (int i = 0; i < charArr.Length; i++)
+            //{
+            //    Console.WriteLine((int)charArr[i]);
+            //}
+
+
+            ////string kapitalBankPrefix = "4169";
+            ////string abbPrefix = "5522";
+
+            //string userCard = "4169-7388-3689-7924,5588-7839-9392-0834";
+            //string[] userCardList = userCard.Split(',');
+
+
+            //for (int i = 0; i < userCardList.Length; i++)
+            //{
+            //    string userPrefix = userCardList[i].Split("-")[0];
+
+
+            //    if (userPrefix == kapitalBankPrefix)
+            //    {
+            //        Console.WriteLine("Kapital Bank");
+            //        continue;
+            //    }
+            //    if (userPrefix == abbPrefix)
+            //    {
+            //        Console.WriteLine("Beynelxalq Bank");
+            //        continue;
+            //    }
+            //}
+
+
+            //int result = Sum(1, 4, 5, 5, 2, 24, 53, 53, 2, 1, 32, 35, 53, 2, 42);
+            //Console.WriteLine(result);
+
+            string[] studentNameList = new string[] { "Emin", "Turkay", "Teymur", "Revan", "Hemze", "Ferid", "Hemid" };
+            string joinedSL = string.Join('-', studentNameList);
+            Console.WriteLine(joinedSL);
+
+
+            //foreach (var studentName in studentNameList)
+            //{
+            //    Console.WriteLine(studentName);
+            //}
+
+        }
+
+        static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
         }
     }
 }
