d4e9d9cc7f9a487a74e3a6cc2e8ea0934a01f4b7
Emin <emin@example.com>
2024-06-23 15:21:54 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -2,11 +2,101 @@
 
 namespace Lessonxxx
 {
-    internal class Program
+    using System;
+    using System.Text;
+
+    public class Program
     {
-        static void Main(string[] args)
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            //Console.WriteLine("Sozleri daxil edin");
+            //string words = Console.ReadLine();
+            //string[] wordsArray = words.Split(',');
+            //for (int i = 0; i < wordsArray.Length; i++)
+            //{
+            //    if (wordsArray[i].Length > 5)
+            //    {
+            //        Console.WriteLine(wordsArray[i]);
+            //    }
+            //}
+            //int result2 = FindMin(18, 55, 3, 31, 88, 1, -5);
+            //Console.WriteLine(result2);
+
+            //int result = FindAverage(15, 4, 8, 3, 10, 8, 22);
+            //Console.WriteLine(result);
+
+            //int result1 = FindMin(55, 47, 3, 12, 21, 99, 0);
+            //Console.WriteLine(result1);
+
+            //string[] words = { "Emin", "Turkay", "Teymur", "Revan", "Hemze", "Ferid", "Hemid" };
+            //string character = "+";
+
+            //string result = CustomJoin(words, character);
+            //Console.WriteLine(result);
+            // }
+
+            // public static string CustomJoin(string[] words, string character)
+            // {
+            //    if (words.Length > 500)
+            //    {
+            //        StringBuilder sb = new StringBuilder();
+            //        for (int i = 0; i < words.Length; i++)
+            //        {
+
+            //            sb.Append(words[i]);
+            //        }
+            //        return sb.ToString();
+            //    }
+            //    else
+            //    {
+            //        string result = "";
+            //        for (int i = 0; i < words.Length; i++)
+            //        {
+            //            result += words[i];
+            //            if (i < words.Length - 1)
+            //            {
+            //                result += character;
+            //            }
+            //        }
+            //        return result;
+            //    }
+
+            //}
+            //static int FindMin(params int[] numList)
+            //{
+            //    Array.Sort(numList);
+            //    return numList[0];
+            //}
+
+            //static int FindAverage(params int[] numList)
+            //{
+
+            //    int sum = 0;
+            //    int bolme = 0;
+            //    for (int i = 0; i < numList.Length; i++)
+            //    {
+            //        sum += numList[i];
+            //        bolme = sum / numList.Length;
+            //    }
+            //    return bolme;
+
+            //}
+            //static int FindMin(params int[] numList)
+            //{
+            //    int minValue = numList[0];
+            //    for (int i = 0; i < numList.Length; i++)
+            //    {
+            //        if (minValue > numList[i])
+            //        {
+            //            minValue = numList[i];
+            //        }
+
+            //    }
+            //    return minValue;
         }
+
     }
-}
+}  
+
