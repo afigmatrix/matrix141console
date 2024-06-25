b18f1478499aedb0768fdf7e4bc2df833777fe20
afiqmehdizada <afig.matrix@gmail.com>
2024-06-25 19:39:34 +0400

    Task created

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,40 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vergul ile ayrilan deyerleri daxil edin !");
+            string userInput = Console.ReadLine();
+
+            if (!userInput.Contains(','))
+            {
+                Console.WriteLine("Duzgun melumat daxil etmediniz!");
+                return;
+            }
+
+            string[] splittedData = userInput.Split(',');
+
+            if (splittedData.Length > 10)
+            {
+                Console.WriteLine("Duzgun melumat daxil etmediniz!");
+                return;
+            }
+
+            string[] newArray = new string[10];
+
+            for (int i = 0; i < splittedData.Length; i++)
+            {
+                if (splittedData[i].Length > 5)
+                {
+                    newArray[i] = splittedData[i];
+                    Console.WriteLine($"newArr[{i}] = spliteddData[{i}]");
+                }
+                else
+                {
+                    Console.WriteLine($"newArr[{i}] = null");
+
+                }
+            }
+
+
         }
     }
 }
