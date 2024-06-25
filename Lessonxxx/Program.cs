3f08f093c24d2513bac4a2d5ff935b6532ef5372
Vuqar Musayev <168574307+vuqarmusayev1995@users.noreply.github.com>
2024-06-25 18:50:44 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,53 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Task1
+            //string oldWord = "hi";
+            //string newWord = "thanks";
+
+            //Console.WriteLine(ReplaceStr("hi", "m"));
+            //Console.WriteLine(ReplaceStr("thanks","ks"));
+
+            //static int ReplaceStr(string oldWord, string newWord)
+            //{
+            //    if (oldWord.Contains(newWord))
+            //    {
+            //        return oldWord.Length;
+            //    }
+            //    else
+            //    {
+            //        return -1;
+
+            //    }
+            //}
+            #endregion
+
+            #region Task2
+            //int num1 = 20;
+            //int num2 = 10;
+            //string operation = "/";
+
+            //double result = Calculate(num1, num2, operation);
+            //Console.WriteLine(result);
+
+            //static double Calculate(int num1, int num2, string operation)
+            //{
+            //    switch (operation)
+            //    {
+            //        case "+":
+            //            return num1 + num2;
+            //        case "-":
+            //            return num1 - num2;
+            //        case "*":
+            //            return num1 * num2;
+            //        case "/":
+            //            return num1 / num2;
+            //        case "%":
+            //            return num1 % num2;   
+            //            break;
+            //        default:
+            //            throw new Exception("duzgun operator daxil et!");
+            #endregion
         }
     }
 }
