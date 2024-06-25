ef9bf3023ea015b960493aaf8833621b9ebd1c1d
Hemze <hmztarverdiyev@gmail.com>
2024-06-25 19:01:50 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,55 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wrld!");
+            //    string[] words = { "Emin", "Turkay", "Teymur", "Revan", "Hemze", "Ferid", "Hemid" };
+            //    string characters = "-";
+
+            //    string result = CustomJoin(words, characters);
+            //    Console.WriteLine(result);
+
+            //}
+            //static string CustomJoin(string[] words, string characters)
+            //{
+            //    string birlesme = string.Join(characters, words);
+            //    return birlesme;
+
+
+
+
+
+            //            int result = FindAverage(1, 2, 3);
+            //            Console.WriteLine(result);
+
+
+            //        }
+            //        static int FindAverage(params int[] numList)
+            //        {
+            //            int sum = 0;
+            //            int sum2 = 0;
+            //            for (int i = 0; i < numList.Length; i++)
+            //            {
+            //                sum += numList[i];
+            //                sum2 = sum / numList.Length;
+
+            //            }
+            //            return sum2;
+
+            //            int result1 = FindMin(45,3,100);
+            //            Console.WriteLine(result1);
+
+            //        }
+            //        static int FindMin(params int[] numList)
+            //        {
+            //            Array.Sort(numList);
+            //            return numList[0];
+            //        }
+            //    }
+            //}
+
+
+
+
+
         }
     }
 }
