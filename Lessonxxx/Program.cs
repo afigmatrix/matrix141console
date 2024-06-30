8faf3fac2785608e648ad97192cc06f815e4237a
Vuqar Musayev <168574307+vuqarmusayev1995@users.noreply.github.com>
2024-07-01 00:52:38 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,125 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region task1Arr
+
+            //int[,] ikiOlculu =
+            //{
+            //    { 1, 2,7,9 },
+            //    { 2, 3,6,8 },
+
+            //};
+            //int count = 0;
+            //for (int i = 0; i < ikiOlculu.GetLength(0); i++)
+            //{
+
+            //    for (int a = 0; a < ikiOlculu.GetLength(1); a++)
+            //    {
+            //        if (ikiOlculu[i, a] % 2 == 0)
+            //        {
+
+            //            count++;
+            //        }
+            //    }
+            //}
+            //int[] tekOlculu = new int[count];
+
+            //int indeks = 0;
+
+            //for (int i = 0; i < ikiOlculu.GetLength(0); i++)
+            //{
+
+            //    for (int b = 0; b < ikiOlculu.GetLength(1); b++)
+            //    {
+            //        if (ikiOlculu[i, b] % 2 == 0)
+            //        {
+
+            //            tekOlculu[indeks] = ikiOlculu[i, b];
+            //            indeks++;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("2-e bolunen ededler:");
+
+            //foreach (int cavab in tekOlculu)
+            //{
+
+            //    Console.WriteLine(cavab + " ");
+            //}
+            #endregion
+
+            #region tash2Arr
+
+            //Console.WriteLine("Cumle yazin: ");
+            //string senteces = Console.ReadLine();
+
+            //string[] herfler = {
+
+            //};
+
+
+
+            #endregion
+
+            #region task3Arr
+
+            //Console.WriteLine("Vergulle ayrilan ededleri daxil edin:(,)");
+            //string input = Console.ReadLine();
+
+            //string[] nums = input.Split(',');
+
+            //Array.Reverse(nums);
+
+            //foreach (string num in nums)
+            //{
+            //   Console.WriteLine(num + " ");
+            //}
+            #endregion
+
+            #region task4Arr
+
+            //Console.Write("Birinci array elementleri yaz: ");
+            //string herf1 = Console.ReadLine();
+            //string[] arr1 = herf1.Split(',');
+
+            //Console.Write("ikinci array elementleri yaz: ");
+            //string herf2 = Console.ReadLine();
+            //string[] arr2 = herf2.Split(",");
+
+            //var ortaqElement = arr1.Intersect(arr2);
+
+            //Console.WriteLine();
+            //foreach (var element in ortaqElement)
+            //{
+            //    Console.WriteLine($"Ortaq elementler: {element}");
+            //}
+            #endregion
+
+            #region task5Arr
+
+            //string[] cars = { "Volvo", "BMW", "Mercedes", "Lada","Totota" };
+            //Array.Sort(cars);
+            //foreach (string car in cars)
+            //{
+            //    Console.WriteLine(car);
+            //}
+
+            //**********************
+
+            //int[] nums = { 1, 9, 5, 7, 3, 2, 0, 6 };
+            //Array.Sort(nums);
+            //foreach (int i in nums)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Array.Reverse(nums);
+            //    foreach (int i in nums) {
+            //    Console.WriteLine(i);   
+            //}
+
+            #endregion
+
         }
     }
 }
