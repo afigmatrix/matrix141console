7525195ebf334632ae08f4bb98fbf9247bdc40f0
afiqmehdizada <afig.matrix@gmail.com>
2024-07-02 21:09:10 +0400

    Done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,63 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Iphone iphone = new Iphone("JBL");
+            Iphone iphone2 = new Iphone();
+            Console.WriteLine(iphone.Camera);
+        }
+
+
+    }
+    public class Iphone : Phone
+    {
+        public string Camera;
+        public string MacroCamera;
+
+        public Iphone(string camera)
+        {
+            Camera = camera;
+        }
+
+        public Iphone()
+        {
+            
+        }
+
+    }
+
+
+
+
+
+
+
+    public class Phone
+    {
+        public string Marka;
+        public string Model;
+        public string Color;
+        public int BataryCapasity;
+        public bool StudentCount;
+
+        public void Call()
+        {
+            Console.WriteLine("Phone call");
         }
     }
+
+ 
+
+    public class Ericson : Phone
+    {
+        public string Lid;
+    }
+
+    public class Monaliza
+    {
+        public string Name;
+        public int Wight;
+    }
+
+
+
 }
