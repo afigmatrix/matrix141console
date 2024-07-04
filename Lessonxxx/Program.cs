9f2611f57dbfa13fa4937b95ce459ca68770a3b2
TeymurAkhmadov <teymur.ahmadov96@gmail.com>
2024-07-04 20:03:21 +0400

    commit ModificationName

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -49,61 +49,60 @@ namespace Lessonxxx
 
         //Task2
         #region
-        //static void Main(string[] args)
-        //{
-        //    Bmw b = new Bmw("530i");
-        //    b.Brand = "BMW";
-        //    b.ModificationName = "530i";
-        //    b.Color = "Ag";
-        //    b.DoorCount = 4;
-        //    b.graduationYear = 2017;
-        //    b.BanType = "Sedan";
-        //    b.Engine = "2.0 l/249 a.g./Benzin";
-        //    b.Distance = "150.000 km";
-        //    b.GearBox = "Avtomat";
+        static void Main(string[] args)
+        {
+            Bmw b = new Bmw("530i");
+            b.Brand = "BMW";
+            b.Color = "Ag";
+            b.DoorCount = 4;
+            b.graduationYear = 2017;
+            b.BanType = "Sedan";
+            b.Engine = "2.0 l/249 a.g./Benzin";
+            b.Distance = "150.000 km";
+            b.GearBox = "Avtomat";
 
-        //    Console.WriteLine($"Marka: {b.Brand}\nModel: {b.ModificationName}\nReng: {b.Color}\nQapi sayisi: {b.DoorCount}\n" +
-        //        $"Buraxilis ili: {b.graduationYear}\nBan Novu: {b.BanType}\nMuherrik: {b.Engine}\n" +
-        //        $"Yurus: {b.Distance}\nSuretler Qutusu: {b.GearBox}");
-        //}
+            Console.WriteLine($"Marka: {b.Brand}\nModel: {b.ModificationName}\nReng: {b.Color}\nQapi sayisi: {b.DoorCount}\n" +
+                $"Buraxilis ili: {b.graduationYear}\nBan Novu: {b.BanType}\nMuherrik: {b.Engine}\n" +
+                $"Yurus: {b.Distance}\nSuretler Qutusu: {b.GearBox}");
+        }
 
-        //public class Car
-        //{
-        //    public int DoorCount;
-        //    public string Color;
-        //    public string Brand;
-        //    public int graduationYear;
-        //    public string BanType;
-        //    public string Engine;
-        //    public string Distance;
-        //    public string GearBox;
+        public class Car
+        {
+            public int DoorCount;
+            public string Color;
+            public string Brand;
+            public int graduationYear;
+            public string BanType;
+            public string Engine;
+            public string Distance;
+            public string GearBox;
 
-        //    public bool IsSport;
-
-            
-        //    public void Start()
-        //    {
-
-        //    }
-
-        //    public void Stop()
-        //    {
-
-        //    }
+            public bool IsSport;
 
 
-        //}
+            public void Start()
+            {
 
-        //public class Bmw : Car
-        //{
-        //    public bool isModified;
-        //    public string ModificationName;
+            }
 
-        //    public Bmw(string ModificationName)
-        //    {
-        //        ModificationName = ModificationName;
-        //    }
-        //}
+            public void Stop()
+            {
+
+            }
+
+
+        }
+
+        public class Bmw : Car
+        {
+            public bool isModified;
+            public string ModificationName;
+
+            public Bmw(string modificationName)
+            {
+                ModificationName = modificationName;
+            }
+        }
         #endregion
 
     }
