4fc29dc2f1230fdca93927435310f2cb9ded367c
Turkay Novruzlu <tnovruzlu7682@gmail.com>
2024-07-05 16:08:54 +0400

    first commit

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Lessonxxx
 {
@@ -6,7 +7,74 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region task1
+            //Bird bird = new Bird();
+            //bird.weight = 4;
+            //bird.isWild = false;
+            //bird.Eyecolor = "blue";
+            //bird.Height = 25;
+
+            //Lion lion = new Lion();
+            //lion.isWild = true;
+            //lion.Eyecolor = "green";
+            //lion.weight = 100;
+            //lion.Height = 70;
+
+            #endregion
+            #region task2
+            bmw bmw = new bmw("restyling");
+            bmw.isModified = true;
+            #endregion
+
         }
     }
+    public class Animal
+    {
+        public int weight;
+        public int Height;
+        public string Eyecolor;
+        public bool isWild;
+        public void Run()
+        {
+
+        }
+    }
+    public class Bird : Animal
+    {
+        public void AbilityToFly()
+        {
+
+        }
+    }
+    public class Lion : Animal
+    {
+        public void Jump()
+        {
+
+        }
+    }
+    public class car
+    {
+        public bool isSport;
+        public string color;
+        public int DoorCount;
+        public void start()
+        {
+
+        }
+        public void stop()
+        {
+
+        }
+    }
+    public class bmw : car
+    {
+        public bool isModified;
+        public string Modificationname;
+        public bmw(string modificationname)
+        {
+            Modificationname = modificationname;
+        }
+          
+    }     
 }
