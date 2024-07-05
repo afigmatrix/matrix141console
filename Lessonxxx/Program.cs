9c688e148d92a34079fccfe751bae094f80c20d4
Emin <emin@example.com>
2024-07-05 15:17:55 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,80 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Bird eagle = new Bird();
+            eagle.weight = 35;
+            eagle.height = 53;
+            eagle.eyeColor = "brown";
+            eagle.isWield = false;
+            eagle.AbiltyToFly();
+
+            Lion lion = new Lion();
+            lion.weight = 600;
+            lion.height = 160;
+            lion.eyeColor = "blue";
+            lion.isWield = false;
+            lion.Jump();
+
+            BMW bmw = new BMW("Alphina");
+            bmw.isSport = true;
+            bmw.isModified = true;
+            bmw.color = "black";
+            bmw.doorCount = 2;
+            bmw.Start();
+            bmw.Stop();
+
+
+        }
+        public class Animal
+        {
+            public int weight;
+            public int height;
+            public string eyeColor;
+            public bool isWield;
+
+            public void Run()
+            {
+            
+            }
+        }
+        public class Bird : Animal
+        {
+            public void AbiltyToFly()
+            {
+
+            }
+        }
+        public class Lion : Animal
+        {
+            public void Jump()
+            {
+
+            }
+        }
+        public class Car
+        {
+            public bool isSport;
+            public string color;
+            public int doorCount;
+
+            public void Start()
+            {
+
+            }
+            public void Stop()
+            {
+
+            }
+        }
+        public class BMW : Car
+        {
+            public bool isModified;
+            public string ModificationName;
+
+            public BMW(string modificationName)
+            {
+                ModificationName = modificationName;
+            }
         }
     }
 }
