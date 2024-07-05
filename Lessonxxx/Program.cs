e62c140a4fd8aff8768de5355dd27c736c32cdbd
afiqmehdizada <afig.matrix@gmail.com>
2024-07-05 19:37:31 +0400

    Done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,32 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int myAge = 18;
+            Financial financial = new Financial();
+            financial.SetAge(17);
+            financial.PropAge = 18;
+            Console.WriteLine(financial.GetAge());
+            Console.WriteLine(financial.PropAge);
+        }
+    }
+
+    public class Financial
+    {
+        private int _age; //18 < 
+        public int PropAge { get { return _age; } set { _age = value; } }
+        public int GetAge()
+        {
+            if (DateTime.Now.Day == 5 )
+            {
+                return _age;
+            }
+            return -1;
+
+        }
+
+        public void SetAge(int age)
+        {
+            _age = age;
         }
     }
 }
