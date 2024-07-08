36c72303ece6fcf3103c5bf93390c9c7d53d7617
Hemid Ismayilov <gamid.ismailov.001@gmail.com>
2024-07-08 21:47:45 +0400

    Done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,56 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+        public class SocialNetwork
+        {
+            public string Email;
+            private string Password;
+            public bool IsActive;
+        }
+        public class Instagram : SocialNetwork
+        {
+            private string Images;
+            public string PropImages
+            {
+                get { return Images; }
+                set
+                {
+                    if (Images.Length >= 7)
+                    {
+                        Images = value;
+                    }
+                    else
+                    {
+                        Images = null;
+                    }
+                }
+            }
+        }
+        public class Facebook : SocialNetwork
+        {
+            public string Post;
+            public string Comment;
+            public string Propcommnet
+            {
+                get { return Comment; }
+                set
+                {
+                    if (true)
+                    {
+                        Comment = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Comment can only be set once");
+                    }
+                }
+            }
+            public Facebook(string post)
+            {
+                Post = post;
+            }
         }
     }
 }
