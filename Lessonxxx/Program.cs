47f2d88f1196815585d4089f120ffb207330e449
Hemze <hmztarverdiyev@gmail.com>
2024-07-09 16:34:30 +0400

    .

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -7,113 +7,70 @@ namespace Lessonxxx
         static void Main(string[] args)
         {
             #region
-            animal animal = new animal();
-            animal.height = 100;
-            animal.weight = 12;
-            animal.isWild = true;
-            animal.eyEcolor = "black";
-            animal.run();
-            Console.WriteLine();
+            Instagram ınstagram = new Instagram();
 
-            bird bird = new bird();
-            bird.height = 16;
-            bird.weight = 17;
-            bird.isWild = true;
-            bird.eyEcolor = "blue";
-            bird.ucmaq = true;
-            bird.AbilityToFly();
-
-            Console.WriteLine();
-
-            lion lion = new lion();
-            lion.height = 16;
-            lion.weight = 18;
-            lion.isWild = true;
-            lion.eyEcolor = "brown";
-            lion.king = true;
-            lion.jump();
-
-            Console.WriteLine();
 
 
         }
-        public class animal
+        public class SocialNetwork
         {
-
-            public int weight;
-            public int height;
-            public string eyEcolor;
-            public bool isWild;
-            public void run()
-            {
-                Console.WriteLine(" qacmaq");
-            }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public bool IsActive { get; set; }
         }
-
-
-        public class bird : animal
-
+        public class Instagram : SocialNetwork
         {
-            public string eyEcolor;
-            public bool isWild;
-            public int weight;
-            public int height;
-            public bool ucmaq;
-
-            public void AbilityToFly()
+            private string _Images;
+            public string Images
             {
-                Console.WriteLine();
-            }
+                get
+                {
+                    return _Images;
+                }
+                set
+                {
+                    if (value.Length > 7)
+                    {
+                        _Images = value;
+                    }
+                    else
+                    {
+                        _Images = null;
+                    }
 
-        }
-
-        public class lion : animal
-        {
-
-
-            public int weight;
-            public int height;
-            public string eyEcolor;
-            public bool isWild;
-            public bool king;
-
-            public void jump()
-            {
-                Console.WriteLine();
+                }
             }
             #endregion
+
+
+
             #region
-            Car car = new Car();
-            
-        }
-        public class Car
-        {
-            public bool İsSport;
-            public string Color;
-            public int DoorCount;
-
-            public void start()
+            public class Fcaebook : SocialNetwork
             {
+                private string _post;
+                private string _comment;
+                private bool _commentSet;
+                public string post
+                {
+                    get
+                    {
+                        return _post;
 
+                    }
+
+
+                    set
+                    {
+                        if (!_commentSet)
+                        {
+                            _comment = value;
+                            _commentSet = true;
+                        }
+                    }
+                }
+                #endregion
             }
-            public void stop()
-            {
-
-            }
-        }
-        public class bmw : Car
-        {
-            bool isModified;
-            string ModificationName;
-            public void moid(string ModificationName)
-            {
-                ModificationName = ModificationName;
-            }
-#endregion
-
-
-
-
         }
     }
 }
+    
