0ca84984bb32ebc7dbc959099bf772519ed205d9
Vuqar Musayev <168574307+vuqarmusayev1995@users.noreply.github.com>
2024-07-12 18:51:30 +0400

    bbb

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -13,108 +13,109 @@ namespace Lessonxxx
 
             #region Boxing-Unboxing
 
-            //// 1) Boxing
-            //int n = 15;
-            //object obj = n;
-            //// 2) Boxing
-            //bool b = true;
-            //object ob = b;
-            //// 3) Boxing
-            //float f = 1.9f;
-            //object o = f;
-            //// 4) Unboxing
-            //string str = "Name";
-            //object c = str;
-            //int i = (int)c;
-            //// 5) Unboxing
-            //object d = 0;
-            //bool e = (bool)d;
+            // 1) Boxing
+            int n = 15;
+            object obj = n;
+            // 2) Boxing
+            bool b = true;
+            object ob = b;
+            // 3) Boxing
+            float f = 1.9f;
+            object o = f;
+            // 4) Unboxing
+            string str = "Name";
+            object c = str;
+            int a = (int)c;
+            // 5) Unboxing
+            object d = 0;
+            bool e = (bool)d;
             #endregion
 
 
             #region Object Array
 
-            //object[] obj = new object[] { 1, 2, "a", "b", 5, "cd", 7, "vm", 9, "z" };
-            //int[] ints = new int[10];
-            //string[] strs = new string[10];
-            //int IndexInt = 0;
-            //int IndexStr = 0;
+            object[] objc = new object[] { 1, 2, "a", "b", 5, "cd", 7, "vm", 9, "z" };
+            int[] ints = new int[10];
+            string[] strs = new string[10];
+            int IndexInt = 0;
+            int IndexStr = 0;
 
-            //foreach (var item in obj)
-            //{
-            //    if (item is int intNum)
-            //    {
-            //        ints[IndexInt] = (int)item;
-            //        IndexInt++;
-            //    }
-            //}
+            foreach (var item in objc)
+            {
+                if (item is int intNum)
+                {
+                    ints[IndexInt] = (int)item;
+                    IndexInt++;
+                }
+            }
 
 
 
-            //foreach (var item in obj)
-            //{
-            //    if (item is string strWord)
-            //    {
-            //        strs[IndexStr] = (string)item;
-            //        IndexStr++;
-            //    }
-            //}
+            foreach (var item in objc)
+            {
+                if (item is string strWord)
+                {
+                    strs[IndexStr] = (string)item;
+                    IndexStr++;
+                }
+            }
 
-            //Console.WriteLine("Integers:");
+            Console.WriteLine("Integers:");
 
-            //for (int i =  0; i < IndexInt; i++)
-            //{
-            //    Console.WriteLine(ints[i]);
-            //}
+            for (int i = 0; i < IndexInt; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
 
-            //Console.WriteLine("Strings:");
+            Console.WriteLine("Strings:");
 
-            //for (int i = 0; i < IndexStr; i++)
-            //{
-            //    Console.WriteLine(strs[i]);
-            //}
+            for (int i = 0; i < IndexStr; i++)
+            {
+                Console.WriteLine(strs[i]);
+            }
 
             #endregion
 
 
             #region Result ref
 
-            //int Result = 0;
+            int Result = 0;
 
-            //Calculator(5, 7, ref Result);
+            Calculator(5, 7, ref Result);
 
-            //Console.WriteLine($"Total: {Result}");
+            Console.WriteLine($"Total: {Result}");
 
-            //static int Calculator(int num1, int num2, ref int Total)
-            //{
-            //    Total = num1 + num2;
+            static int Calculator(int num1, int num2, ref int Total)
+            {
+                Total = num1 + num2;
 
-            //    return Total;
-            //}
+                return Total;
+            }
             #endregion
-
             #region Instance sayi
 
-        //    Car car = new Car();
-        //    Car car1 = new Car();
-        //    Car car2 = new Car();
-        //    Car car3 = new Car();
+            Car car = new Car();
+            Car car1 = new Car();
+            Car car2 = new Car();
+            Car car3 = new Car();
 
-        //    Console.WriteLine(Car.MyCarNum1);
+            Console.WriteLine(Car.MyCarNum1);
 
         }
-        //public class Car
-        //{
-        //    public static int MyCarNum1 { get; set; }
-        //    public int MyCarNum2 { get; set; }
 
-        //    public  Car()
-        //    {
-        //        MyCarNum1++;
-        //        MyCarNum2++;
-        //    }
-        //}
+        public class Car
+        {
+            public static int MyCarNum1 { get; set; }
+            public int MyCarNum2 { get; set; }
+
+            public Car()
+            {
+                MyCarNum1++;
+                MyCarNum2++;
+            }
+        }
         #endregion
+
 
     }
 }
