246df1d8be037a42031ec871c87105b8353fc8a9
Hemid Ismayilov <gamid.ismailov.001@gmail.com>
2024-07-12 13:32:49 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,74 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Task1
+            //int a = 1; // boxing
+            //object inta = a;
+            //bool b = false;
+            //object boolb = b;
+            //char c = 'a'; 
+            //object charc = c;
+            //float d = 1.23f;
+            //object floatd = d;
+            //decimal e = 1.2543m;
+            //object decimale = e;
+
+            //object aa = 1;
+            //int intaa = (int)aa;
+            //object bb = true;
+            //bool boolbb = (bool)bb;
+            //object cc = 'd';
+            //char charcc = (char)cc;
+            //object dd = 1.53f;
+            //float floatdd = (float)dd;
+            //object ee= 1.5432m;
+            //decimal decimalee= (decimal)ee;
+            #endregion Task1
+            #region Task2
+            //object[] objArray = new object[10] { 1, "salam", 200, "sagol", 350, "Matrix", 213, "Hemid", 2342, "dqwe"};
+            //int[] intArray = new int[objArray.Length];
+            //string[] stringArray = new string[objArray.Length];
+
+
+            //int intCount = 0;
+            //int stringCount = 0;
+
+
+            //foreach (var item in objArray)
+            //{
+
+            //    if (item is int)
+            //    {
+            //        intArray[intCount] = (int)item;
+            //        intCount++;
+            //    }
+            //    else if (item is string)
+            //    {
+            //        stringArray[stringCount] = (string)item;
+            //        stringCount++;
+            //    }
+            //}
+            //Console.WriteLine("Int array:");
+            //for (int i = 0; i < intCount; i++)
+            //{
+            //    Console.WriteLine(intArray[i]);
+            //}
+
+            //Console.WriteLine("String array:");
+            //for (int i = 0; i < stringCount; i++)
+            //{
+            //    Console.WriteLine(stringArray[i]);
+            //}
+            #endregion Task2
+            //int Result = 0; 
+            //Calculate(5, 3, ref Result); 
+            //Console.WriteLine($"Result: {Result}");
         }
+
+        //static void Calculate(int num1, int num2, ref int result)
+        //{
+        //    result = num1 + num2; 
+        //}
     }
+    
 }
