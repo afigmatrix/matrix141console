3bbd05f57c8dd36efe19da47102aa3e8b7f1d1a7
Vuqar Musayev <168574307+vuqarmusayev1995@users.noreply.github.com>
2024-07-15 22:44:25 +0400

    aa

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,65 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rabbit rabbit = new Rabbit();
+            rabbit.Run();
+            rabbit.Eat();
+
+            Dog dog = new Dog();
+            dog.Run();
+            dog.Eat();
+
+            Cat cat = new Cat();
+            cat.Run();
+            cat.Eat();
         }
+
+        public class Animal
+        {
+            public virtual void Run()
+            {
+                Console.WriteLine();
+            }
+            public virtual void Eat()
+            {
+                Console.WriteLine();
+            }
+        }
+        public class Rabbit : Animal
+        {
+            public void Run()
+            {
+                Console.WriteLine("Rabbit runs");
+            }
+            public void Eat()
+            {
+                Console.WriteLine("Rabbit eats");
+            }
+
+        }
+        public class Dog : Animal
+        {
+            public void Run()
+            {
+                Console.WriteLine("Dog runs");
+            }
+            public void Eat()
+            {
+                Console.WriteLine("Dog eats");
+            }
+        }
+        public class Cat : Animal
+        {
+            public void Run()
+            {
+                Console.WriteLine("Cat runs");
+            }
+            public void Eat()
+            {
+                Console.WriteLine("Cat eats");
+            }
+        }
+
     }
 }
+
