0648640d8129bbfdb3b781987f90823421c8a78a
TeymurAkhmadov <teymur.ahmadov96@gmail.com>
2024-07-15 17:27:55 +0400

    commit CarAbstract

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -4,8 +4,7 @@ namespace Lessonxxx
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+       
             //Task Animal
             #region
             //static void Main(string[] args)
@@ -80,98 +79,94 @@ namespace Lessonxxx
 
             //Task Car
             #region
-            //static void Main(string[] args)
-            //{
+            static void Main(string[] args)
+            {
 
-            //    Bmw bmw = new Bmw(2, "Black");
-            //    Mercedes mercedes = new Mercedes(4, "White");
+                Bmw bmw = new Bmw(2, "Black");
+                Mercedes mercedes = new Mercedes(4, "White");
 
-            //    Console.WriteLine("BMW actions:");
-            //    bmw.Gas();
-            //    bmw.Break();
-            //    bmw.FlashOptimizer();
-            //    Console.WriteLine($"DoorCount:{bmw.DoorCount}\nColorName: {bmw.ColorName}");
-
-
-
-            //    Console.WriteLine("-----------------------------");
-
-            //    Console.WriteLine("\nMercedes actions:");
-            //    mercedes.Gas();
-            //    mercedes.Break();
-            //    mercedes.FlashOptimizer();
-            //    Console.WriteLine($"DoorCount:{mercedes.DoorCount}\nColorName: {mercedes.ColorName}");
-            //}
-
-            //public  class Car
-            //{
-            //    public  int DoorCount { get; set; }
-            //    public  string ColorName { get; set; }
-
-            //    public virtual void Gas()
-            //    {
-            //        Console.WriteLine("Gas");
-            //    }
-            //    public virtual void Break()
-            //    {
-            //        Console.WriteLine("Break");
-            //    }
-            //    public virtual void FlashOptimizer()
-            //    {
-            //        Console.WriteLine("CAR - FlashOptimizer");
-            //    }
-
-            //}
+                Console.WriteLine("BMW actions:");
+                bmw.Gas();
+                bmw.Break();
+                bmw.FlashOptimizer();
+                Console.WriteLine($"DoorCount:{bmw.DoorCount}\nColorName: {bmw.ColorName}");
 
 
 
-            //public  class Bmw : Car
-            //{
+                Console.WriteLine("-----------------------------");
 
-            //    public Bmw(int doorCount, string colorName)
-            //    {
-            //        DoorCount = doorCount;
-            //        ColorName = colorName;
-            //    }
+                Console.WriteLine("\nMercedes actions:");
+                mercedes.Gas();
+                mercedes.Break();
+                mercedes.FlashOptimizer();
+                Console.WriteLine($"DoorCount:{mercedes.DoorCount}\nColorName: {mercedes.ColorName}");
+            }
 
+       public abstract class Car
+        {
+            public int DoorCount { get; set; }
+            public string ColorName { get; set; }
 
-            //    public override void Gas()
-            //    {
-            //        Console.WriteLine("BMW Gas");
-            //    }
-            //    public override void Break()
-            //    {
-            //        Console.WriteLine("BMW Break");
-            //    }
-            //    public override void FlashOptimizer()
-            //    {
+            public abstract void Gas();
 
-            //        Console.WriteLine("Bmw - FlashOptimizer");
-            //    }
-            //}
-            //public  class Mercedes :Car
-            //{
-            //    public Mercedes(int doorCount, string colorName)
-            //    {
-            //        DoorCount = doorCount;
-            //        ColorName = colorName;
-            //    }
-            //    public override void Gas()
-            //    {
-            //        Console.WriteLine("Mercedes Gas");
-            //    }
-            //    public override void Break()
-            //    {
-            //        Console.WriteLine("Mercedes Break");
+            public abstract void Break();
+           
+            public virtual void FlashOptimizer()
+            {
+                Console.WriteLine("CAR - FlashOptimizer");
+            }
 
-            //    }
-            //    public override void FlashOptimizer()
-            //    {
-
-            //        Console.WriteLine("Mercedes - FlashOptimizer");
-            //    }
-            //}
-            #endregion
         }
+
+
+
+        public class Bmw : Car
+        {
+
+            public Bmw(int doorCount, string colorName)
+            {
+                DoorCount = doorCount;
+                ColorName = colorName;
+            }
+
+
+            public override void Gas()
+            {
+                Console.WriteLine("BMW Gas");
+            }
+            public override void Break()
+            {
+                Console.WriteLine("BMW Break");
+            }
+            public override void FlashOptimizer()
+            {
+
+                Console.WriteLine("Bmw - FlashOptimizer");
+            }
+        }
+        public class Mercedes : Car
+        {
+            public Mercedes(int doorCount, string colorName)
+            {
+                DoorCount = doorCount;
+                ColorName = colorName;
+            }
+            public override void Gas()
+            {
+                Console.WriteLine("Mercedes Gas");
+            }
+            public override void Break()
+            {
+                Console.WriteLine("Mercedes Break");
+
+            }
+            public override void FlashOptimizer()
+            {
+
+                Console.WriteLine("Mercedes - FlashOptimizer");
+            }
+        }
+        #endregion
     }
 }
+
