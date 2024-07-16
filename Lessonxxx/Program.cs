304c02d70dd35a6ede92bc26028a24f4d17b1c8c
Hemze <hmztarverdiyev@gmail.com>
2024-07-16 18:57:25 +0400

    .

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,99 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region
+            Rabbit rabbit = new Rabbit();
+            rabbit.Run();
+            rabbit.Eat();
+            Dog dog = new Dog();
+            dog.Run();
+            dog.Eat();
+            Cat cat = new Cat();
+            cat.Run();
+            dog.Run();
+
+
+        }
+        public abstract class Animal
+        {
+            public abstract void Eat();
+            public abstract void Run();
+        }
+        public class Rabbit : Animal
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("yemek");
+            }
+            public override void Run()
+            {
+                Console.WriteLine("qacmaq");
+            }
+
+        }
+
+        public class Dog : Animal
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("yemek");
+            }
+            public override void Run()
+            {
+                Console.WriteLine("qacmaq");
+            }
+
+        }
+        public class Cat : Animal
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("yemek");
+            }
+            public override void Run()
+            {
+                Console.WriteLine("qacmaq");
+            }
+            #endregion
+            #region
+            BMW bmw = new BMW();
+
+
+
+        }
+        public abstract class Car
+        {
+            public abstract int QapıSayı { get; }
+            public abstract string RəngAdı { get; }
+
+
+            public abstract void Gas();
+            public abstract void Break();
+
+
+            public void FlashOptimizator()
+            {
+                Console.WriteLine("");
+            }
+
+        }
+        public class BMW : Car
+        {
+            public override string RəngAdı => "qara";
+            public override int QapıSayı => 2;
+
+            public override void Gas()
+            {
+                Console.WriteLine(".");
+            }
+
+            public override void Break()
+            {
+                Console.WriteLine("");
+            }
+            #endregion
+
+
         }
     }
 }
