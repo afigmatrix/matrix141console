962620683b21a508c6f79958318b0d99d31d787b
Vuqar Musayev <168574307+vuqarmusayev1995@users.noreply.github.com>
2024-07-18 00:20:25 +0400

    ll

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,26 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime today = DateTime.Now;
+            DayOfWeek dayOfWeek =today.DayOfWeek;
+
+            Console.WriteLine(WeekDayName((Week)dayOfWeek));
         }
+        static string  WeekDayName(Week day) 
+        {
+            return day.ToString();
+        }
+        public enum Week
+        {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7,
+        }
+            
+
     }
 }
