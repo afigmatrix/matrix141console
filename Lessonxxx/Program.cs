d79cfb24f2538e3b50734d1e5a628089a421050c
Hemid Ismayilov <gamid.ismailov.001@gmail.com>
2024-07-23 16:23:49 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lessonxxx
 {
@@ -6,7 +7,110 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Task1
+            //ArrayList array = new ArrayList();
+            //array.Add(1);
+            //array.Add(2);
+            //array.Add("salam");
+            //array.Add(3);
+            //array.Add("sagol");
+            //array.TrimToSize();
+            //Console.WriteLine("index yazin");
+            //int i = int.Parse(Console.ReadLine());
+            //try
+            //{
+            //    var value = array[i];
+            //    Console.WriteLine($"indexin deyeri: {value}");
+            //}
+            //catch (ArgumentOutOfRangeException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("element cixarma prosesi bitdi");
+            //}
+            #endregion
+            #region Task2
+            //try
+            //{
+            //    int num1 = int.Parse(Console.ReadLine());
+            //    int num2 = int.Parse(Console.ReadLine());
+            //    int res = num1 + num2;
+            //    Console.WriteLine($"ededlerin cemi: {res}");
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            #endregion
+            #region Task3
+            //Dictionary<string, string> kitabca = new Dictionary<string, string>();
+            //kitabca.Add("Anar", "0556789034");
+            //kitabca.Add("Samir", "0556367090");
+            //kitabca.Add("Elsad", "0556582064");
+            //while (true)
+            //{
+            //    Console.WriteLine("Ad daxil edin");
+            //    string name = Console.ReadLine();
+            //    if (name.Contains("exit"))
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine("Telefon nomresini daxil edin");
+            //    string phone = Console.ReadLine();
+            //    if (!kitabca.ContainsKey(name))
+            //    {
+            //        kitabca.Add(name, phone);
+            //        Console.WriteLine($"{name} ve {phone} elave olundu");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Bu ad daxil edilib");
+            //    }
+
+            //}
+            //foreach (var item in kitabca)
+            //{
+            //    Console.WriteLine($"Ad: =>{item.Key} Telefonu => {item.Value}");
+            //}
+            //while (true)
+            //{
+            //    Console.WriteLine("Ad daxil edin nomresini tapmaq ucun");
+            //    string sname = Console.ReadLine();
+            //    if (sname.Contains("exit"))
+            //    {
+            //        break;
+            //    }
+            //    if (kitabca.ContainsKey(sname))
+            //    {
+            //        Console.WriteLine($"Ad => {sname} Telefonu: => {kitabca[sname]} ");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Bu ad tapilmadi");
+            //    }
+            //}
+            #endregion
+            #region Task4
+            //HashSet<string> email = new HashSet<string>();
+            //email.Add("Hemid@gmail.com");
+            //email.Add("Samir@gmail.com");
+            //email.Add("Elvin@gmail.com");
+            //string semail = Console.ReadLine();
+            //if (email.Contains(semail))
+            //{
+            //    Console.WriteLine("Bu email movcuddur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bu email movcud deyil");
+            //}
+            #endregion
         }
     }
 }
