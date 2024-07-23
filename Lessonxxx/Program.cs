e023fa0f0162a681e9f241f47ada5210befb73c2
TeymurAkhmadov <teymur.ahmadov96@gmail.com>
2024-07-23 16:06:53 +0400

    commit

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Lessonxxx
 {
@@ -6,7 +8,90 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Task1 
+            #region
+            //int userInput = int.Parse(Console.ReadLine());
+
+            //int[] arr = new int[userInput];
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    arr[i] = i * 2;
+            //}
+            //try
+            //{
+            //    Console.WriteLine(arr[userInput]);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Yanlis index daxil edilib");
+            //}
+            #endregion
+
+            //Task2
+
+            #region
+            //    int number1 = default;
+            //    int number2 = default;
+
+            //    try
+            //    {
+            //        number1 = int.Parse(Console.ReadLine());
+            //        number2 = int.Parse(Console.ReadLine());
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("Duzgun deyer daxil edin");
+
+            //    }
+            //    finally
+            //    {
+            //        Console.WriteLine("Hesab bitmisdir!");
+            //    }
+
+            //    Console.WriteLine(Sum(number1,number2));
+            //}
+
+            //static int Sum(int num1,int num2)
+            //{
+            //    return num1 + num2;
+            //}
+
+            #endregion
+
+            //Task3
+            #region
+            //yoxdu * *
+           //        -
+            #endregion
+
+            //Task4
+            #region
+            //HashSet<string> UserEmail = new HashSet<string>();
+            //UserEmail.Add("info@mail.ru");
+            //UserEmail.Add("Egov@gmail.com");
+            //UserEmail.Add("fixed@mail.az");
+
+            //Console.WriteLine("Email daxil edin ");
+            //string email = Console.ReadLine();
+
+            //foreach (var item in UserEmail)
+            //{
+            //    if (UserEmail.Contains(email))
+            //    {
+            //        Console.WriteLine("Bu e-mail siyahida var: " + email);
+            //        break;
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Bu e-mail siyahida yoxdur: " + email);
+            //        return;
+            //    }
+            //}
+            #endregion
         }
     }
-}
+  }
+
+
