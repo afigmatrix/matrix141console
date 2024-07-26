e429e3b7e7fa6ae59f4b210e3cf16c09749a0242
Hemid Ismayilov <gamid.ismailov.001@gmail.com>
2024-07-26 12:29:00 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lessonxxx
 {
@@ -6,7 +9,73 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region Task1
+            //List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //var kvadrat = list.Select(m => m*m).ToList();
+            //foreach (var item in kvadrat)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Task2
+            //List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //var cut = list.Where(m => m % 2 == 0).ToList();
+            //foreach (int i in cut)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region Task3
+            //List<string> list = new List<string> { "salam","sagol", "alma", "gul", "telelfon", "dehset" };
+            //var sira = list.OrderBy(x => x).ToList();
+            //foreach (var s in sira)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            #endregion
+            #region Task4
+            //List<string> list = new List<string> { "salam", "sagol", "alma", "gul", "telelfon", "dehset" };
+            //var uzunluq = list.Select(x => x.Length);
+            //foreach ( var x in uzunluq)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            #endregion
+            #region Task5
+            //List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //var cem = list.Sum(x => x);
+            //Console.WriteLine(cem);
+            #endregion
+            #region Task6
+            //List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //var average = list.Average();
+            //Console.WriteLine(average);
+            #endregion
+            #region Task7
+            //List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };
+            //var filtr = list.Where(m => m > 3).ToList();
+            //foreach (var item in filtr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Task8
+            //List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10 };
+            //var sira = list.OrderByDescending(x => x).ToList();
+            //foreach (int i in sira)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region Task9
+            //List<string> list = new List<string>() { "salam", "sagol", "alma", "gul", "telelfon", "dehset" };
+            //var soz = list.Where(list=>list.Length >5).ToList();
+            //foreach(string s in soz)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            #endregion
         }
     }
 }
