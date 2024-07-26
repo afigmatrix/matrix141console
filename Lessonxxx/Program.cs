f8ab1b0fa729c7e67006e64d09c4d424dc2445d0
Farid <kerimovferid1000@gmail.com>
2024-07-26 16:32:32 +0400

    done

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,72 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region task1
+            //List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //var SquareNumbers=Numbers.Select(x=>x*x).ToList();
+            //foreach(int numbers in SquareNumbers)
+            //{
+            //    Console.WriteLine($"{Math.Sqrt(numbers)}-in kvadrat qarsiligi:{numbers}");
+            //}
+            #endregion
+            #region task2
+            //List<int> Numbers = new List<int>() { 1, 2, 4, 5, 5, 6, 7, 8, 9 };
+            //var EvenNumbers = Numbers.Where(m => m % 2 == 0).ToList();
+            //foreach (var numbers in EvenNumbers)
+            //{
+            //    Console.WriteLine(numbers);
+            //}
+            #endregion
+            #region task3
+            //List<string> list = new List<string>() { "Fərid", "Eldar", "Orxan", "Ismayıl", "Kənan" };
+            //var SortedNames=list.OrderBy(x => x).ToList();
+            //foreach (var names in SortedNames)
+            //{
+            //    Console.WriteLine(names);
+            //}
+            #endregion
+            #region task4
+            //List<string> list = new List<string>() { "Fərid", "Eldar", "Orxan", "Ismayıl", "Kənan" };
+            //var NamesLength=list.Select(x => x.Length).ToList();
+            //foreach (var names in NamesLength)
+            //{
+            //    Console.WriteLine(names);
+            //}
+            #endregion
+            #region task5
+            //List<int> Nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //var SumNums = Nums.Sum(x => x);
+            //Console.WriteLine(SumNums);
+            #endregion
+            #region task6
+            //List<int> Nums = new List<int>() { 1, 2, 3,4,5,6,7,8,9 };
+            //var AverageNumbers = Nums.Average(m => m);
+            //Console.WriteLine(AverageNumbers);
+            #endregion
+            #region task7
+            //List<int> Nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //var FiltredNumbers=Nums.Where(m=>m>3).ToList();
+            //foreach(int nums in FiltredNumbers)
+            //{
+            //    Console.WriteLine(nums);
+            //}
+            #endregion
+            #region task8
+            //List<int> Nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //var DescendedNumbers=Nums.OrderByDescending(x => x).ToList();
+            //foreach (int Numbers in DescendedNumbers)
+            //{
+            //    Console.WriteLine(Numbers);
+            //}
+            #endregion
+            #region task9
+            //List<string> Values = new List<string>() { "swdwbwdbu", "sdwekjdhwud", "sjsj", "sdh", "wdsjwhgdwi" };
+            //var SortedValues=Values.Where(m=>m.Length>5).ToList();
+            //foreach (var values in SortedValues)
+            //{
+            //    Console.WriteLine(values);
+            //}
+            #endregion
         }
     }
 }
