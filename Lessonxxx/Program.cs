ad85737ec23f9cf820dea8c4840c1d78cd1f8056
Hemze <hmztarverdiyev@gmail.com>
2024-07-26 17:29:24 +0400

    .

--- Lessonxxx/Program.cs
+++ Lessonxxx/Program.cs
@@ -6,7 +6,81 @@ namespace Lessonxxx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region
+            //List<int> kvadrat = new List<int> { 2, 4, 6, 8, };
+
+            //var result = kvadrat.
+
+
+            #endregion
+            #region
+            //List<int> cut = new List<int> { 1,2,3,4,5,6,7,8,9};
+            //var result1= cut.Where(a=>a%2==0).ToList();
+
+            //foreach( var a in result1 )
+            //{
+            //    Console.WriteLine(a);
+            //}
+            #endregion
+            #region
+            //List<string> list1 = new List<string> { "davud", "azer", "cavid", "babek", "elman" };
+            //var result23 = list1.OrderBy(sira => sira).ToList();
+
+            //foreach (var sira in result23)
+            //{
+            //    Console.WriteLine(sira);
+            //}
+            #endregion
+            #region
+
+            //List<string> uzunluq = new List<string> { "car", "name", "bus" };
+
+            //var result24 = uzunluq
+
+
+            #endregion
+            #region
+            //List<int> topla = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            //var result12 = topla.Sum(v => v);
+            //Console.WriteLine(" Cemi: " + result12);
+
+            #endregion
+            #region
+            //List<int> ortalama = new List<int>{13, 30, 45, 40, 50 };
+            //var result233 = ortalama.Average(eded => eded);
+
+
+            //Console.WriteLine("Ortalama: " + result233);
+            #endregion
+            #region
+            //List<int> Boyukededler = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //var result123 = Boyukededler.Where(p => p > 3).ToList();
+
+            //foreach (var p in result123)
+            //{
+            //    Console.WriteLine(p);
+            //}
+            #endregion
+            #region
+            //List<int> azalanededler = new List<int> { 7, 5, 8, 4, 2, 1, 6, 9, 10 };
+            //var result567 = azalanededler.OrderByDescending(eded45 => eded45).ToList();
+
+            //foreach (var eded45 in result567)
+            //{
+            //    Console.WriteLine(eded45);
+            //}
+            #endregion
+            #region
+
+            //List<string> uzunmetn = new List<string> { "name", "car", "string", " homeless" };
+
+            //var result10 = uzunmetn.Where(metn=> metn.Length > 5).ToList();
+            //foreach (var metn in result10)
+            //{
+            //    Console.WriteLine(metn);
+            //}
+            #endregion
+
         }
     }
 }
